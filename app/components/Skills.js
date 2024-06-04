5a3b8a1a99cb64a6d744e448426d927cfd43989a
unknown <ericryanbowser@gmail.com>
2024-06-04 02:48:04 -0600

    portfolio updates. add cards and Skills. WIP

---
+++ app/components/Skills.js
@@ -0,0 +1,66 @@
+﻿import React from 'react';
+import random from '../../public/random3.jpg'
+import scrum from '../../public/scrum-gpt.png';
+import skills from '../../public/skills_web.jpg';
+
+export const Skills = [
+    {
+        title: "JavaScript",
+        description: "Proficient in JavaScript and have lots of experience using NPM and Node.js. Knowledgeable in server-side development with Express.js and RESTful APIs using Node as a back-end.",
+        image: scrum
+    },
+    {
+        title: "React",
+        description: "Experienced in building single-page applications with React, classes and hooks. I have worked with, developed, implemented, tested and deployed components into a components library to be used internally.",
+        image: skills
+    },
+    {
+        title: "CSS",
+        description: "Familiar with responsive design and grid layout systems using Bootstrap. Also familiar with React-Bootstrap components and Styled-Components.",
+        image: random
+    },
+    {
+        title: "DotNet",
+        description: "Experienced and proficient using and bootstrapping web / service applications. I have worked extensively with dotnet Standard versions as well as newer dotnet core up to netcore 8.",
+        image: random
+    },
+    {
+        title: "Scripting",
+        description: "Experienced with Bash and Git Bash shells. I have experience scripting automation build scripts and deploy scripts. Git Bash for source control to GitHub. Also, for front-end JavaScript, NPM package manager and build / run scripts.",
+        image: skills
+    },
+    {
+        title: "CI / CD",
+        description: "Continuous integration using various frameworks like TeamCity Build and Octopus Deploy. I have also worked with Azure Devops. I have scripted and implemented a build pipeline that runs automation tests, and runs a build process based on criteria.",
+        image: skills
+    },
+    {
+        title: "Data",
+        description: "I am proficient and experienced using Microsoft Transact SQL. I have written many Stored Procedures, User-Defined functions, Views, Tables and Cursors. Lots and lots of experience querying data and executing functions. Also, on the front-end, I have experience with postgeSQL. Experience migrating data from postgreSQL to Azure CosmosDB.",
+        image: skills
+    },
+];
+
+/*
+export const SkillsListView = () => {
+    return (
+        <div className="container mx-auto py-10">
+            <h2 className="text-3xl font-bold text-center mb-6">My Skills</h2>
+            <div className="row">
+                {skills.map((skill, index) => (
+                    <div key={index} className="col-md-4 col-lg-3 mb-4">
+                        <div className="card bg-white shadow-lg hover:shadow-xl transition duration-200">
+                            <div className="card-body">
+                                <h5 className="card-title text-xl font-semibold">{skill.title}</h5>
+                                <p className="card-text text-gray-700">{skill.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+*/
+
+export default Skills;
--- app/page.js
+++ app/page.js
@@ -1,19 +1,18 @@
 'use client';
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import Head from "next/head";
 import {useRouter} from 'next/navigation';
 import {BsFillMoonStarsFill} from "react-icons/bs";
 import {AiFillGithub, AiFillLinkedin} from "react-icons/ai";
 import {CgMail} from "react-icons/cg";
-import {Link, Element, animateScroll as scroll} from 'react-scroll';
+import {animateScroll as scroll} from 'react-scroll';
 import Image from 'next/image';
-import head from '../public/head-50.jpg';
-import {encrypt} from "encrypt-js-vault";
-import skills from '../public/skills_web.jpg';
-import scrum from '../public/scrum-gpt.png';
-import random from '../public/random3.jpg';
 import {Card} from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import Skills from "./components/Skills";
+import {FiDribbble} from "react-icons/fi";
+import {FaReact} from "react-icons/fa";
+import head2 from '../public/head2.jpg'
+import {TbBrandJavascript} from "react-icons/tb";
 
 /*export default function getServerSideProps() {
     const secretKey = process.env.SMTP_HOST; // Ensure this variable is not prefixed with NEXT_PUBLIC_
@@ -25,24 +24,49 @@ import Button from "react-bootstrap/Button";
 const Portfolio = () => {
     const router = useRouter();
     const [darkMode, setDarkMode] = useState(false);
-/*
-    const originalText = process.env.SMTP_PASS;
-    const encryptedText = encrypt(originalText);
-    console.log('Encrypted Text:', encryptedText);
-*/
-    
-    const scrollToBottom = () => {
-        const element = document.getElementById('')
-        scroll.scrollToBottom(100, 50);
+    /*
+        const originalText = process.env.SMTP_PASS;
+        const encryptedText = encrypt(originalText);
+        console.log('Encrypted Text:', encryptedText);
+    */
+
+    const scrollTo = () => {
+        scroll.scrollMore(900);
     };
-    
+
     const changeRouterToContact = () => {
         router.push('/contact');
     };
-    
+
+    const getCard = (skill = {}) => {
+        return (
+            <div className={'w-full md:w-1/2 lg:w-1/3 p-4'}>
+                <Card
+                    className={'h-50 rounded-lg p-4 md:p-8 transition-all dark:bg-gray-900 dark:border dark:border-gray-800 shadow-xl shadow-cyan-500/20 dark:shadow-lg dark:shadow-cyan-500/30 dark:text-white-400 border-2 border-transparent hover:border-current4'}>
+                    <Card.Title>
+                        <span className={'flex p-5 gap-5 text-center font-burtons'}>
+                            {skill.title === "React" ? <FaReact size={20}/> : null}
+                            {skill.title}
+                        </span>
+                    </Card.Title>
+                    <div
+                        className={'relative mx-auto bg-gradient-to-b from-teal-500 w-60 h-40 py-5 overflow-hidden'}>
+                        <Image src={skill.image} objectFit={'cover'} layout='fill' alt={'headshot'}/>
+                    </div>
+                    <Card.Body>
+                        <Card.Text
+                            style={{height: '420px'}}
+                            className={'text-center p-5'}>
+                            {skill.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+
     return (
-        <div className={darkMode ? "dark" : ""}>
-           
+        <div className={darkMode ? "dark" : "mt-5"}>
             <Head>
                 <title>E.R.B.</title>
                 <meta name={'My Portfolio Site'} content={'Next App Creation'}/>
@@ -56,6 +80,12 @@ const Portfolio = () => {
                             <h3 className={'text-cyan-500'}>E.R.B.</h3>
                         </section>
                         <ul className={'flex items-center'}>
+                            <li className={'px-96'}>
+                                Skills
+                                <button onClick={scrollTo}>
+                                    <FiDribbble size={40}/>
+                                </button>
+                            </li>
                             <li>
                                 <BsFillMoonStarsFill className={'cursor-pointer 2xl'}
                                                      onClick={() => setDarkMode(!darkMode)}/>
@@ -71,8 +101,7 @@ const Portfolio = () => {
                         <h2 className={'text-5xl py-2 text-teal-600 font-medium'}>Eric Ryan Bowser</h2>
                         <h3 className={'text-2xl py-2'}>Full Stack Developer</h3>
                         <p className={'text-md py-5 leading-0 text-gray-800 dark:text-white'}>
-                            Full stack developer with emphasis in React.js on the frontend and C# .NET on the
-                            backend.
+                            Full stack developer with JavaScript, React.js, Node.js and Next.js libraries.
                         </p>
                     </div>
                     <div className={'text-5xl flex justify-center gap-16 p-3 cursor-pointer'}>
@@ -86,76 +115,27 @@ const Portfolio = () => {
                             <CgMail/>
                         </button>
                     </div>
-
                     <div
-                        className={'relative mx-auto bg-gradient-to-b from-teal-500 rounded-2xl w-40 h-40 mt-10 overflow-hidden'}>
+                        className={'relative mx-auto bg-gradient-to-b from-teal-500 rounded-2xl w-60 h-80 mt-10 overflow-hidden'}>
                         <Image src={head2} objectFit={'cover'} layout='fill' alt={'headshot'}/>
                     </div>
-
-                    <h2 className={'text-5xl py-10 text-teal-600 font-medium my-10  text-center'}>Skills</h2>
-                    <section className={'flex justify-evenly text-wrap text-center'}>
-
-                        <Card
-                            style={{width: '18rem'}}
-                            className={'rounded-lg p-4 md:p-8 transition-all bg-white dark:bg-gray-900 dark:border dark:border-gray-800 shadow-xl shadow-cyan-500/20 dark:shadow-lg dark:shadow-cyan-500/30 text-cyan-500 dark:text-white-400 border-2 border-transparent hover:border-current4'}>
-                            <Card.Title>JavaScript</Card.Title>
-                            <div
-                                className={'relative mx-auto bg-gradient-to-b from-teal-500 w-60 h-40 mt-10 overflow-hidden'}>
-                                <Image src={skills} objectFit={'cover'} layout='fill' alt={'headshot'}/>
-                            </div>
-
-                            <Card.Body>
-                                <Card.Text
-                                    className={'text-justify align-middle py-5'}>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card
-                            className={'rounded-lg p-4 md:p-8 transition-all bg-white dark:bg-gray-900 dark:border dark:border-gray-800 shadow-xl shadow-cyan-500/20 dark:shadow-lg dark:shadow-cyan-500/30 text-cyan-500 dark:text-white-400 border-2 border-transparent hover:border-current'}>
-                            <Card.Title>C# .NET</Card.Title>
-                            <div
-                                className={'relative mx-auto bg-gradient-to-b from-teal-500 w-60 h-40 mt-10 overflow-hidden'}>
-                                <Image src={random} objectFit={'cover'} layout='fill' alt={'headshot'}/>
-                            </div>
-
-                            <Card.Body>
-                                <Card.Text
-                                    className={'text-justify align-middle py-5'}>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        <Card
-                            style={{width: '18rem'}}
-                            className={'rounded-lg p-4 md:p-8 transition-all bg-white dark:bg-gray-900 dark:border dark:border-gray-800 shadow-xl shadow-cyan-500/20 dark:shadow-lg dark:shadow-cyan-500/30 text-cyan-500 dark:text-white-400 border-2 border-transparent hover:border-current'}>
-                            <Card.Title>Scrum</Card.Title>
-                            <div
-                                className={'relative mx-auto bg-gradient-to-b from-teal-500 w-60 h-40 mt-10 overflow-hidden'}>
-                                <Image src={scrum} objectFit={'cover'} layout='fill' alt={'headshot'}/>
-                            </div>
-
-                            <Card.Body>
-                                <Card.Text
-                                    className={'text-justify align-middle py-5'}>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                        {/* <img className={''}
-                            src={random2} 
-                            alt={'skills-random'}/>*/}
+                    <h2 className={'text-5xl py-10 text-black dark:text-white font-medium text-center'}>Skills</h2>
+                    <section className={'flex flex-col md:flex-row flex-wrap dark:text-white dark:bg-gray-800 h-228'}>
+                        {Skills.map((skill, index) => {
+                            return (
+                                getCard(skill)
+                            )
+                        })
+                        }
                     </section>
-                    <footer id="footerSection"
-                            className="mt-8 p-4 text-black text-center dark:bg-gray-800 dark:text-white">
-                        <h2>E.R.B. Eric Ryan Bowser</h2>
-                        <p>ericryanbowser@gmail.com</p>
-                    </footer>
                 </section>
             </main>
+            <footer
+                id="footerSection"
+                className="fixed-bottom mt-8 p-4 mt-xxl-4 text-black text-center dark:bg-gray-800 dark:text-white">
+                <h2>E.R.B. Eric Ryan Bowser</h2>
+                <p>ericryanbowser@gmail.com</p>
+            </footer>
         </div>
     );
 }
