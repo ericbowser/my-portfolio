2f26990ddcd6b88b3e9d6207c67e788c6472db11
Eric Bowser <ericryanbowser@gmail.com>
2025-08-24 06:38:42 -0600

    config updates

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 module.exports = defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: 'localhost',
+    https: {}
+  },
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+      ]
+      }
+  }
 })
